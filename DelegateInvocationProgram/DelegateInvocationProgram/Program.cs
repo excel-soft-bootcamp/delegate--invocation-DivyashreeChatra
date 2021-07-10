f0ead1bfde91e9a1dd71df74327e6edcf9be7b0b
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-07-10 12:52:50 +0530

    updated

---
+++ DelegateInvocationProgram/DelegateInvocationProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateInvocationProgram
+{
+    class Program
+    {
+
+        static double Calculate(int x, double y) { return x + y; }
+
+        void Update(int newNumber) { System.Console.WriteLine(newNumber); }
+
+        void Delete(string key) { }
+        public void M1(){ }
+        public void M2(string data) { }
+        public int M3(int data1) { return data1; }
+        public bool M4(string data,string newdata) { return true; }
+
+        static void FunctionsAsAnArguments(Action obj, Action<string> obj2, Func<int, int> obj3, Func<string, string, bool> obj4,Func<int,int,bool>obj5,Action<int>obj6,Action<string>obj7)
+        {
+
+        
+
+
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Action m1ptr = new Action(obj.M1);
+            Action<string> m2ptr = new Action<string>(obj.M2);
+            Func<int,int>m3ptr = new Func<int,int>(obj.M3);
+            Func<string, string, bool> m4ptr = new Func<string, string, bool>(obj.M4);
+            Func<int, double, double> calculate = new Func<int, double, double>(Program.Calculate);
+            Action<int> update = new Action<int>(obj.Update);
+            Action<string> delete = new Action<string>(obj.Delete);
+        }
+    }
+}
